7134d5ed18d191937a0b70b0a893131489db4173
PSADMIN <psadmin@psadmins-air-8.attlocal.net>
2021-05-16 19:50:49 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+//assign ten variables values
+var a = 2
+var b = 3
+var c = 4
+var d = 5
+var e = 1
+var f = 3
+var g = 6
+var h = 5
+var i = 8
+var j = 10
+// declare three new variables for storing the results
+var av_first,av_second,av_final
+// assign the average of our first five variable to another variable
+av_first = (a+b+c+d+e)/5
+//assign the average of last 5 variable t another variable
+av_second=(f+g+h+i+j)/5
+// assign average of average toa new variable
+av_final=(av_first+av_second)/2
+// output av_final to the console
+console.log(av_first,av_second,av_final)
